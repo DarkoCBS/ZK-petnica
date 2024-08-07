dfbd4edd6f06968e1522d810c3c8c8d82bcdefd2
Kila369 <lazar.joksimovic7@gmail.com>
2024-08-08 01:22:01 +0200

    Change reedem membership to start on init

--- my-app/apps/web-app/src/app/proofs/[id]/page.tsx
+++ my-app/apps/web-app/src/app/proofs/[id]/page.tsx
@@ -15,78 +15,69 @@ export default function ProofsPage() {
 
     const params = JSON.parse(decodeURIComponent(searchParams.id as unknown as string))
 
-    const verifyMembership = useCallback(async () => {
-        if (params) {
-            setLoading(true)
+    useEffect(() => {
 
-            try {
-                let response: any = {}
-
-                const { merkleTreeDepth, merkleTreeRoot, nullifier, message, points, groupId } = params
-
-                if (process.env.OPENZEPPELIN_AUTOTASK_WEBHOOK) {
-                    response = await fetch(process.env.OPENZEPPELIN_AUTOTASK_WEBHOOK, {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            abi: Feedback.abi,
-                            address: process.env.FEEDBACK_CONTRACT_ADDRESS,
-                            functionName: "verifyMembership",
-                            functionParameters: [merkleTreeDepth, merkleTreeRoot, nullifier, message, points]
+        const verifyMembership = async () => {
+            if (params) {
+                setLoading(true)
+    
+                try {
+                    let response: any = {}
+    
+                    const { merkleTreeDepth, merkleTreeRoot, nullifier, message, points, groupId } = params
+    
+                    if (process.env.OPENZEPPELIN_AUTOTASK_WEBHOOK) {
+                        response = await fetch(process.env.OPENZEPPELIN_AUTOTASK_WEBHOOK, {
+                            method: "POST",
+                            headers: { "Content-Type": "application/json" },
+                            body: JSON.stringify({
+                                abi: Feedback.abi,
+                                address: process.env.FEEDBACK_CONTRACT_ADDRESS,
+                                functionName: "verifyMembership",
+                                functionParameters: [merkleTreeDepth, merkleTreeRoot, nullifier, message, points]
+                            })
                         })
-                    })
-                } else {
-                    response = await fetch("/api/feedback", {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            feedback: message,
-                            merkleTreeDepth,
-                            merkleTreeRoot,
-                            nullifier,
-                            points,
-                            mintTo: "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720",
-                            groupId,
+                    } else {
+                        response = await fetch("/api/feedback", {
+                            method: "POST",
+                            headers: { "Content-Type": "application/json" },
+                            body: JSON.stringify({
+                                feedback: message,
+                                merkleTreeDepth,
+                                merkleTreeRoot,
+                                nullifier,
+                                points,
+                                mintTo: "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720",
+                                groupId,
+                            })
                         })
-                    })
-                }
-
-                if (response.status === 200) {
-                    setLogs(`Your feedback has been posted 🎉`)
-                    setSuccess(true)
-                } else {
+                    }
+    
+                    if (response.status === 200) {
+                        setLogs(`Your feedback has been posted 🎉`)
+                        setSuccess(true)
+                    } else {
+                        setLogs("Some error occurred, please try again!")
+                        setFail(true)
+                    }
+                } catch (error) {
+                    console.error(error)
+    
                     setLogs("Some error occurred, please try again!")
                     setFail(true)
+                } finally {
+                    setLoading(false)
                 }
-            } catch (error) {
-                console.error(error)
-
-                setLogs("Some error occurred, please try again!")
-                setFail(true)
-            } finally {
-                setLoading(false)
             }
         }
-    }, [params, setLogs])
+
+        verifyMembership()
+
+    }, [])
 
     return (
         <>
-            <h2>Proofs</h2>
-
-            <p>
-                Semaphore members can anonymously
-            </p>
-
             <div className="divider"></div>
-
-            {!(fail || success) ? (
-                <div>
-                    <button className="button" onClick={verifyMembership} disabled={_loading}>
-                        <span>Redeem ticket</span>
-                        {_loading && <div className="loader"></div>}
-                    </button>
-                </div>
-            ) : <></>}
 
             {fail ? (
                 <div>Failed!</div>
